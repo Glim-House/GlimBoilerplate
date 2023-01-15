94bedba1b39fa479e81bcc20ff63b51677cd960f
aj-neoito <ajo.a@neoito.com>
2023-01-15 22:29:34 +0530

    multi platform files

---
+++ windows/GlimBoilerplate/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::GlimBoilerplate::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::GlimBoilerplate::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
